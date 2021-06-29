4530f5fe853bcf0b1e2025d7ec308af2c26f69fc
cts <cts@qq.com>
2021-06-29 10:10:48 +0800

    add livedata  add vertor asset 矢量图

--- app/src/main/java/com/example/jetpackdemo/MainActivity.java
+++ app/src/main/java/com/example/jetpackdemo/MainActivity.java
@@ -4,15 +4,19 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
     MyViewModel myViewModel;
-    private Button mZan;
+    private ImageButton mZan;
     private Button mCai;
     private TextView mResult;
 
@@ -24,22 +28,29 @@ public class MainActivity extends AppCompatActivity {
 
         myViewModel = new ViewModelProvider(this).get(MyViewModel.class);
 
+        myViewModel.getLiveData().observe(this, new Observer<Integer>() {
+            @Override
+            public void onChanged(Integer o) {
+                mResult.setText(o+"");
+            }
+        });
+
         initView();
     }
 
     private void initView() {
-        mZan = (Button) findViewById(R.id.zan);
+        mZan = (ImageButton) findViewById(R.id.zan);
         mCai = (Button) findViewById(R.id.cai);
         mResult = (TextView) findViewById(R.id.result);
         Log.d(TAG, "initView: ");
         //默认设置
-        mResult.setText(myViewModel.value+"");
+        mResult.setText( myViewModel.getLiveData().getValue()+"");
         mZan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                myViewModel.value++;
+                myViewModel.setLiveData(1);
 
-                mResult.setText(myViewModel.value+"");
+
             }
         });
 
--- app/src/main/java/com/example/jetpackdemo/MyViewModel.java
+++ app/src/main/java/com/example/jetpackdemo/MyViewModel.java
@@ -1,10 +1,32 @@
 package com.example.jetpackdemo;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class MyViewModel extends ViewModel {
 
-    public int value;
+    MutableLiveData<Integer> liveData;
 
+    public MutableLiveData getLiveData() {
+        if (liveData==null){
 
+            liveData  = new MutableLiveData<>();
+            liveData.setValue(0);
+        }
+
+        return liveData;
+    }
+
+    public void setLiveData(int value) {
+        if (liveData==null){
+
+            liveData  = new MutableLiveData<>();
+            liveData.setValue(0);
+        }
+        else{
+            liveData.setValue(liveData.getValue()+value);
+
+        }
+
+    }
 }
